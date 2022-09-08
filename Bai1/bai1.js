7179809bd12cf4514ec4d4c16b368530cfa928f4
tung <vuthanhtungtd2@gmail.com>
2022-09-08 09:56:39 +0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ Bai1/bai1.js
@@ -0,0 +1,24 @@
+let i = 10;
+let f =20.5;
+let b = true;
+let s = "Ha noi" ;
+document.write('i = ' + i )
+document.write('<br/>')
+document.write('f='+f)
+document.write('b=' + b)
+document.write('<br/>')
+document.write('s=' +s )
+let width = 20 ;let height = 10
+let area = width * height
+document.write("bla" + area)
+
+
+let a= prompt("nhập a")
+let b= prompt("nhập b")
+
+let TB = a%b
+if(TB == 0){
+    alert("a chia het cho b")
+} else {
+    alert("Giá trị nhập vào là số lẻ")
+}
