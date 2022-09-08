ef99ec62cd01cbaefb24cf3811147e4b8b06eb2a
tung <vuthanhtungtd2@gmail.com>
2022-09-08 10:23:52 +0700

    [Thực hành] Sử dụng toán tử

---
+++ Bai4/Bai4.js
@@ -0,0 +1,12 @@
+let width = prompt("Nhập width");
+let height= prompt("Nhập Height");
+
+document.write("chiều rộng"+ width)
+document.write('<br>')
+document.write("chiều dài"+ height);
+
+document.write('<br>')
+let area = width * height;
+
+
+document.write("Diện tích" + area);
